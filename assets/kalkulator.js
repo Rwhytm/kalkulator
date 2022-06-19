b437ddd4ac453125845a499f98ab16f504c260fb
Rwhytm <rajuwahyudi842@gmail.com>
2022-06-19 21:05:23 +0700

    add storage and function in calculator

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,1 +1,37 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecond: false,
+}
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecond = false;
+}
+
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll('.button');
+
+
+for (const button of buttons) {
+    button.addEventListener('click', (event) => {
+        const target = event.target;
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    })
+}
+
+function updateNumber() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
